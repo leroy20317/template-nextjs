b97e8e75c657c0c20ead8fbeada2a10f4abaee26
leroy <gmail@leroytop.com>
2025-06-25 14:36:18 +0800

    feat: add prettier-plugin-tailwindcss for improved styling support

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,19 @@
+/*
+ * @Author: leroy
+ * @Date: 2024-10-31 11:23:01
+ * @LastEditTime: 2025-06-25 14:35:51
+ * @Description: prettier config
+ */
 /** @type {import('prettier').Options} */
-const prettierConfig =  {
+const prettierConfig = {
   singleQuote: true,
   trailingComma: 'all',
   printWidth: 100,
   proseWrap: 'never',
   endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindStylesheet: './src/pages/global.css',
+  tailwindFunctions: ['classNames'],
 };
 
 export default prettierConfig
